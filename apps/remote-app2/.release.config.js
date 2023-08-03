c3f491510aaa8a47f5c1fea259cc3a34fe0c43df
Vadim Ermakov <vadim.ermakov@altenar.com>
2023-08-03 17:52:38 +0400

    init

---
+++ apps/remote-app2/.release.config.js
@@ -0,0 +1,36 @@
+const name = 'remote-app2';
+const path = `apps/${name}`;
+
+module.exports = {
+  name: name,
+  branches: ['release'],
+  pkgRoot: `dist/${path}`,
+  tagFormat: '${version}',
+  commitPaths: [`${path}/*`],
+  assets: [`${path}/README.md`, `${path}/CHANGELOG.md`],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${path}/CHANGELOG.md`,
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: `VERSION=\${nextRelease.version} && echo $VERSION > ${path}/.version`,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${path}/CHANGELOG.md`],
+        message:
+          `chore(release): ${name} \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}`,
+      },
+    ],
+  ],
+};
