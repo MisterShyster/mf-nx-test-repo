8b7a4cb8bf01e3664f0561134318d851d1ab0318
Vadim Ermakov <vadim.ermakov@altenar.com>
2023-08-03 14:33:55 +0400

    init app code

---
+++ apps/remote-app1/src/app/app.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <div>Hello</div>
+      <NxWelcome title="remote-app1" />
+    </StyledApp>
+  );
+}
+
+export default App;
